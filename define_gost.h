1bdcf40554051a932d16bd405e05d85bbd5f29aa
DethleSS <rik546@mail.ru>
2019-03-11 19:56:39 +0300

    New Gost standart

---
+++ define_gost.h
@@ -0,0 +1,57 @@
+#ifndef DEFINE_GOST_H
+#define DEFINE_GOST_H
+#include <string>
+#include <iostream>
+#include <vector>
+
+#define zeloe_chislo int
+#define bolshoe_zeloe_shislo long long int
+#define drobnoe_chislo float
+#define bolshoe_drobnoe_chislo double
+#define simval char
+#define stroka std::string
+#define dinamicheskiy_massive std::vector<int>
+#define B bool
+#define chto_to_vivodit void
+#define avtomat auto
+#define spisok enum
+#define shablon template
+#define semeistvo class
+#define imya typename
+#define yslovie void
+#define drugoe else
+#define za_loopa for
+#define popa while
+#define noviy new
+#define eto this
+#define dam public
+#define ne_dam private
+#define zashishau protected
+#define vernuti return
+#define kavichka_odinarnaya "'"
+#define kavichka_dvoinaya ""
+#define dva_dvoetochya ::
+#define zapaytaya ,
+#define tochka_zapyatoy ;
+#define elochka_vlevo <<
+#define elochka_v_pravo >>
+#define ravno =
+#define neravno !=
+#define bolshe >
+#define menshe <
+#define strelochka ->
+#define zvezdochka *
+#define vivod cout
+#define podcluchiti #include
+#define kvodratnaya_skobka_levaya [
+#define kvodratnaya_skobka_pravaya ]
+#define skobochka_levaya (
+#define skobochka_pravaya )
+#define figurnaya_skobopchka_levaya {
+#define figurnaya_skobopchka_pravaya }
+
+
+
+
+
+#endif // DEFINE_GOST_H
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <QCoreApplication>
+#include "define_gost.h"
+#include <iostream>
+avtomat main skobochka_levaya zeloe_chislo argc zapaytaya simval *argv kvodratnaya_skobka_levaya kvodratnaya_skobka_pravaya skobochka_pravaya strelochka zeloe_chislo
+figurnaya_skobopchka_levaya
+    QCoreApplication a skobochka_levaya argc zapaytaya argv skobochka_pravaya tochka_zapyatoy
+    std dva_dvoetochya vivod elochka_vlevo "Hello world" tochka_zapyatoy
+    vernuti a.exec skobochka_levaya skobochka_pravaya tochka_zapyatoy
+figurnaya_skobopchka_pravaya
